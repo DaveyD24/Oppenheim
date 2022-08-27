1aa1bcada00f7c084edf5f6637e13852dc29792a
DaveyD24 <69884791+DaveyD24@users.noreply.github.com>
2022-08-27 22:54:37 +1000

    Player camera switching
    I will refactor this into base class shenanigans soon

--- Assets/GreenController.cs
+++ Assets/GreenController.cs
@@ -11,6 +11,8 @@ public class GreenController : MonoBehaviour
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
+    bool active = true;
+
     private void Start()
     {
         controller = gameObject.AddComponent<CharacterController>();
@@ -18,27 +20,45 @@ public class GreenController : MonoBehaviour
 
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
+        if (active)
         {
-            playerVelocity.y = 0f;
+            groundedPlayer = controller.isGrounded;
+            if (groundedPlayer && playerVelocity.y < 0)
+            {
+                playerVelocity.y = 0f;
+            }
+
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            controller.Move(move * Time.deltaTime * playerSpeed);
+
+            if (move != Vector3.zero)
+            {
+                gameObject.transform.forward = move;
+            }
+
+            // Changes the height position of the player..
+            if (Input.GetButtonDown("Jump") && groundedPlayer)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            }
+
+            playerVelocity.y += gravityValue * Time.deltaTime;
+            controller.Move(playerVelocity * Time.deltaTime);
         }
+    }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
+    public void Activate()
+    {
+        active = true;
+    }
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
+    public void Deactivate()
+    {
+        active = false;
+    }
 
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+    public bool isActive()
+    {
+        return active;
     }
 }
---
+++ Assets/SwitchManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchManager : MonoBehaviour
+{
+
+    [SerializeField] public RedController red;
+    [SerializeField] public BlueController blue;
+    [SerializeField] public GreenController green;
+    [SerializeField] public YellowController yellow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RotatePlayer();
+        }
+    }
+
+    void RotatePlayer()
+    {
+        //bygr
+        if (blue.isActive())
+        {
+            DeactivateAll();
+            yellow.Activate();
+        }
+        else if (yellow.isActive())
+        {
+            DeactivateAll();
+            green.Activate();
+        }
+        else if (green.isActive())
+        {
+            DeactivateAll();
+            red.Activate();
+        }
+        else if (red.isActive())
+        {
+            DeactivateAll();
+            blue.Activate();
+        }
+
+    }
+
+    void DeactivateAll()
+    {
+        red.Deactivate();
+        blue.Deactivate();
+        green.Deactivate();
+        yellow.Deactivate();
+    }
+}
