9dcb1205059cf54a36a74c341d0fad18fe43548c
Jenny <43059884+IkarosJenny@users.noreply.github.com>
2022-08-31 14:05:43 +1000

    cant detect input?

--- Assets/Scripts/SoldierMovement.cs
+++ Assets/Scripts/SoldierMovement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class SoldierMovement : MonoBehaviour
 {
 
+    //public float Rigidbody3D rb;
     public CharacterController soldierController;
-
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10;
     public float speed = 6f;
 
     void Update() {
@@ -14,10 +17,19 @@ public class SoldierMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        
         if (direction.magnitude >= 0.1f) {
-
-
+            Debug.Log("Im moving");
             soldierController.Move(direction * speed * Time.deltaTime);
         }
+
+        if (Input.GetKeyDown(KeyCode.F)) {
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+        }
     }
+
+    
+
+    
 }
