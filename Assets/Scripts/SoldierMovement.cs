dbcccbd7bbe9314a0bb9bcfe139297d8021c1178
Jenny <43059884+IkarosJenny@users.noreply.github.com>
2022-09-10 12:11:23 +1000

    ^

--- Assets/Scripts/SoldierMovement.cs
+++ Assets/Scripts/SoldierMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SoldierMovement : MonoBehaviour
 {
+    //Swimming
+    public bool isSwimming;
+    public float swimSpeed;
+    public Transform target;
 
     //public float Rigidbody3D rb;
     public CharacterController soldierController;
@@ -11,22 +15,55 @@ public class SoldierMovement : MonoBehaviour
     public GameObject bulletPrefab;
     public float bulletSpeed = 10;
     public float speed = 6f;
+    Rigidbody rigidbody;
 
-    void Update() {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+    void Awake()
+    {
+        rigidbody = GetComponent<rigidbody>();
+    }
 
+    void FixedUpdate() {
+        if (isSwimming != true)
+        {
+            if (rigidbody.useGravity != true)
+            {
+                rigidbody.useGravity = true;
+            }
+            //input
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+            //move
+            if (direction.magnitude >= 0.1f)
+            {
+                Debug.Log("Im moving");
+                soldierController.Move(direction * speed * Time.deltaTime);
+            }
+
+            //shoot
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+                bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+            }
+        }
+        else 
+        {
+            if (rigidbody.useGravity == true)
+            {
+                rigidbody.useGravity = false;
+            }
+            if (Input.GetAxisRaw("Vertical") > 0) 
+            {
+                transform.position += target.forward * swimSpeed * Time.deltaTime;
+            }
+            if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                transform.position -= target.forward * swimSpeed * Time.deltaTime;
+            }
+        }
         
-        if (direction.magnitude >= 0.1f) {
-            Debug.Log("Im moving");
-            soldierController.Move(direction * speed * Time.deltaTime);
-        }
-
-        if (Input.GetKeyDown(KeyCode.F)) {
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
-        }
     }
 
     
---
+++ Assets/Scripts/WaterScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Player") && other.GetComponent<CharacterController>() != null)
+        {
+            CharacterController soldier = other.GetComponent<CharacterController>();
+            soldier.isSwimming = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && other.GetComponent<CharacterController>() != null)
+        {
+            CharacterController soldier = other.GetComponent<CharacterController>();
+            soldier.isSwimming = false;
+        }
+    }
+}
