8524db30125e83e4e6122b2177e3dc9f5ee7c1b0
Jenny <43059884+IkarosJenny@users.noreply.github.com>
2022-09-17 13:49:47 +1000

    .

---
+++ Assets/FuelScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelScript : MonoBehaviour
+{
+    private Image fuelBar;
+    public float currentFuel;
+    private float maxFuel = 100f;
+    //PlayerControllerScript player
+
+    private void Start() 
+    {
+        fuelBar = GetComponent<Image>();
+        //player = FindObjectOfType<PlayerControllerScript>()
+        
+    }
+
+    private void Update()
+    {
+        //currentFuel = player.fuel;
+        //fuelBar.fillAmount = currentFuel / maxFuel;
+    }
+}
