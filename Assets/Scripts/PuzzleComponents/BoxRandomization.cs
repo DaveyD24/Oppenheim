be740dc60b91beba35528bf2eebb2a065521741b
DJHPersonal <David.J.Hestelow@student.uts.edu.au>
2022-09-21 17:55:31 +1000

    Limit box Direction
    Forced the boxes to only move in either the x or z directions when they are pushed to enable easier movement of them around the level.

--- Assets/Scripts/PuzzleComponents/BoxRandomization.cs
+++ Assets/Scripts/PuzzleComponents/BoxRandomization.cs
@@ -11,13 +11,14 @@ public class BoxRandomization : MonoBehaviour
     [SerializeField] private Material[] randomMat;
     [SerializeField] private bool bDoRandMat;
     [SerializeField] private bool bDoRandMesh;
+    [SerializeField] private bool bDoRandRotation = false;
     [SerializeField] private MeshFilter[] meshFilters;
     [SerializeField] private MeshRenderer[] meshRenderers;
-    private MeshRenderer textRenderer;
+    private Rigidbody rb;
 
-    // Start is called before the first frame update
     private void Start()
     {
+        Rigidbody rb = GetComponent<Rigidbody>();
         if (bDoRandMesh)
         {
             int randLetter = Random.Range(0, textMeshes.Length - 1);
@@ -45,5 +46,52 @@ public class BoxRandomization : MonoBehaviour
                 renderer.sharedMaterials = sharedMat;
             }
         }
+
+        if (bDoRandRotation)
+        {
+            Quaternion rot = Quaternion.Euler(Random.Range(0, 4) * 90, Random.Range(0, 4) * 90, Random.Range(0, 4) * 90);
+            transform.rotation = rot;
+        }
+    }
+
+    /// <summary>
+    /// limit the box to only move one direction when pushed.
+    /// only limit on the x and z global directions.
+    /// </summary>
+    private void OneDirectionMove()
+    {
+        Vector3 velocity = rb.velocity;
+        if (velocity.magnitude > 1)
+        {
+            float xVelocity = Mathf.Abs(rb.velocity.x);
+            float zVelocity = Mathf.Abs(rb.velocity.z);
+            // determine the direction with the most force and freeze the other
+            if (xVelocity > zVelocity)
+            {
+                velocity.z = 0;
+                rb.constraints = RigidbodyConstraints.None;
+                rb.constraints = RigidbodyConstraints.FreezePositionZ;
+            }
+            else
+            {
+                rb.constraints = RigidbodyConstraints.None;
+                rb.constraints = RigidbodyConstraints.FreezePositionX;
+                velocity.x = 0;
+            }
+
+            rb.angularVelocity = new Vector3(0, 0, 0);
+            rb.velocity = velocity;
+        }
+        else
+        {
+            rb.constraints = RigidbodyConstraints.None;
+        }
+
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        OneDirectionMove();
     }
 }
