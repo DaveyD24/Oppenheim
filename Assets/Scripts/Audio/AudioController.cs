e690f329dadf1a712aae3e3ebab4753d928c10c5
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2022-09-26 19:42:39 +1000

    More Audio Playing Options
    Added Play at Vector3 position and begin playing a sound at a designated time.

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -10,10 +10,74 @@ public class AudioController : MonoBehaviour
 {
 	[SerializeField] HashMap<string, AudioData> HashMap;
 	Dictionary<string, AudioData> Map;
+	Dictionary<int, AudioSource> Unique;
 
 	void Awake()
 	{
 		HashMap.Construct(out Map);
+		Unique = new Dictionary<int, AudioSource>();
+	}
+
+	/// <summary>Plays <paramref name="SoundName"/> at <paramref name="Position"/>.</summary>
+	/// <param name="SoundName">The name of the sound to play.</param>
+	/// <param name="Position">The world coordinates of where this <paramref name="SoundName"/> should play.</param>
+	/// <param name="PlaybackLead">The time in seconds to fast forward.</param>
+	public AudioSource PlayAtPosition(string SoundName, Vector3 Position, float PlaybackLead = 0f)
+	{
+		if (Get(SoundName, out AudioData Data))
+		{
+			Spawn(out GameObject Spawned, false);
+
+			Spawned.transform.position = Position;
+			Data.Construct(Spawned, out AudioSource Source);
+
+			Source.time = PlaybackLead;
+			Source.Play();
+			Destroy(Spawned, Data.ClipDuration());
+
+			return Source;
+		}
+
+		return null;
+	}
+
+	/// <summary>Plays one instance of <paramref name="SoundName"/> with <paramref name="Options"/>.</summary>
+	/// <remarks><b>Uniqueness is relative to this instance of <see cref="AudioController"/>, and NOT UNIQUE globally!</b></remarks>
+	/// <param name="PlaybackLead">The time in seconds to fast forward.</param>
+	public AudioSource PlayUnique(string SoundName, EAudioPlayOptions Options, float PlaybackLead = 0f)
+	{
+		int HashCode = SoundName.GetHashCode();
+		bool bAllowedToPlay = true;
+
+		// If this sound was uniquely played before.
+		if (Unique.ContainsKey(HashCode))
+		{
+			AudioSource Source = Unique[HashCode];
+
+			// If the Source has been destroyed, or is no longer playing, it is allowed to play.
+			if (!Source || !Source.isPlaying)
+			{
+				Unique.Remove(HashCode);
+			}
+			// Otherwise, the sound still playing and is not unique.
+			else
+			{
+				bAllowedToPlay = false;
+			}
+		}
+
+		if (!bAllowedToPlay)
+			return null;
+
+		MaskOptions(Options, out byte OptionsAsByte, out bool bDesroyOnEnd);
+
+		if (IsIllegalInput(OptionsAsByte))
+			return null;
+
+		AudioSource UniqueSource = PlayAudioSignature(SoundName, OptionsAsByte, bDesroyOnEnd, PlaybackLead);
+		Unique.Add(HashCode, UniqueSource);
+
+		return UniqueSource;
 	}
 
 	/// <summary>Plays <paramref name="SoundName"/> with <paramref name="Options"/>.</summary>
@@ -23,38 +87,31 @@ public class AudioController : MonoBehaviour
 	/// <see cref="EAudioPlayOptions.AtTransformPosition"/>,
 	/// or to <see cref="EAudioPlayOptions.FollowEmitter"/>,<br></br>
 	/// and defines whether or not to destroy the AudioSource.</param>
+	/// <param name="PlaybackLead">The time in seconds to fast forward.</param>
 	/// <returns>The <see cref="AudioSource"/> used to play <paramref name="SoundName"/>.</returns>
-	public AudioSource Play(string SoundName, EAudioPlayOptions Options)
+	public AudioSource Play(string SoundName, EAudioPlayOptions Options, float PlaybackLead = 0f)
 	{
-		byte OptionsAsByte = (byte)Options;
-		bool bDestroyOnEnd = OptionsAsByte >> 3 == 1;
+		byte OptionsAsByte;
+		bool bDestroyOnEnd;
+		MaskOptions(Options, out OptionsAsByte, out bDestroyOnEnd);
 
-		OptionsAsByte &= 7;
+		return PlayAudioSignature(SoundName, OptionsAsByte, bDestroyOnEnd, PlaybackLead);
+	}
 
-		if ((OptionsAsByte & 7) == 0)
-		{
-			Debug.LogError("Invalid Options! Must define ONE of Global, AtTransformPosition, or FollowEmitter.");
-			return null;
-		}
-
-		if ((OptionsAsByte & (OptionsAsByte - 1)) != 0)
-		{
-			Debug.LogError("Invalid Options! Must define ONE of Global, AtTransformPosition, or FollowEmitter.");
-			return null;
-		}
-
+	public AudioSource PlayAudioSignature(string SoundName, byte OptionsAsByte, bool bDestroyOnEnd, float PlaybackLead = 0f)
+	{
 		if ((OptionsAsByte & (byte)EAudioPlayOptions.Global) == (byte)EAudioPlayOptions.Global)
 		{
-			return Play(SoundName);
+			return Play(SoundName, PlaybackLead);
 		}
 		else if ((OptionsAsByte & (byte)EAudioPlayOptions.AtTransformPosition) == (byte)EAudioPlayOptions.AtTransformPosition)
 		{
 			Spawn(out GameObject AtPosition, false);
-			return PlayAtTransformPosition(SoundName, AtPosition, bDestroyOnEnd);
+			return PlayAtTransformPosition(SoundName, AtPosition, bDestroyOnEnd, PlaybackLead);
 		}
 		else if ((OptionsAsByte & (byte)EAudioPlayOptions.FollowEmitter) == (byte)EAudioPlayOptions.FollowEmitter)
 		{
-			return PlayFollow(SoundName, gameObject, bDestroyOnEnd);
+			return PlayFollow(SoundName, gameObject, bDestroyOnEnd, PlaybackLead);
 		}
 
 		Debug.LogError($"Could not play {SoundName}!");
@@ -63,10 +120,11 @@ public class AudioController : MonoBehaviour
 
 	/// <summary>Plays <paramref name="SoundName"/> globally (3D Spatial Sound is set to 2D).</summary>
 	/// <param name="SoundName">The name of the Sound to play.</param>
-	public AudioSource Play(string SoundName)
+	/// <param name="PlaybackLead">The time in seconds to fast forward.</param>
+	public AudioSource Play(string SoundName, float PlaybackLead = 0f)
 	{
 		Spawn(out GameObject Spawned, true);
-		AudioSource Global = PlayFollow(SoundName, Spawned, true);
+		AudioSource Global = PlayFollow(SoundName, Spawned, true, PlaybackLead);
 
 		if (Global != null)
 		{
@@ -82,13 +140,15 @@ public class AudioController : MonoBehaviour
 	/// <param name="bDestroyOnEnd">
 	/// <see langword="true"/> to Destroy the new <see cref="GameObject"/>
 	/// once <paramref name="SoundName"/> finishes playing.</param>
-	public AudioSource PlayAtTransformPosition(string SoundName, GameObject Emitter, bool bDestroyOnEnd)
+	/// <param name="PlaybackLead">The time in seconds to fast forward.</param>
+	public AudioSource PlayAtTransformPosition(string SoundName, GameObject Emitter, bool bDestroyOnEnd, float PlaybackLead = 0f)
 	{
 		if (Get(SoundName, out AudioData A))
 		{
 			A.Construct(Emitter, out AudioSource Source);
 
 			Emitter.transform.position = transform.position;
+			Source.time = PlaybackLead;
 			Source.Play();
 
 			if (bDestroyOnEnd)
@@ -107,11 +167,13 @@ public class AudioController : MonoBehaviour
 	/// <see langword="true"/> to destroy the newly added <see cref="AudioSource"/> component
 	/// once <paramref name="SoundName"/> finishes playing.
 	/// </param>
-	public AudioSource PlayFollow(string SoundName, GameObject Emitter, bool bDestroyComponentOnEnd)
+	/// <param name="PlaybackLead">The time in seconds to fast forward.</param>
+	public AudioSource PlayFollow(string SoundName, GameObject Emitter, bool bDestroyComponentOnEnd, float PlaybackLead = 0f)
 	{
 		if (Get(SoundName, out AudioData A))
 		{
 			A.Construct(Emitter, out AudioSource Source);
+			Source.time = PlaybackLead;
 			Source.Play();
 
 			if (bDestroyComponentOnEnd)
@@ -123,6 +185,27 @@ public class AudioController : MonoBehaviour
 		return null;
 	}
 
+	static void MaskOptions(EAudioPlayOptions Options, out byte OptionsAsByte, out bool bDestroyOnEnd)
+	{
+		OptionsAsByte = (byte)Options;
+		bDestroyOnEnd = OptionsAsByte >> 3 == 1;
+		OptionsAsByte &= 7;
+	}
+
+	static bool IsIllegalInput(byte OptionsAsByte)
+	{
+		if ((OptionsAsByte & 7) == 0)
+		{
+			Debug.LogError("Invalid Options! Must define ONE of Global, AtTransformPosition, or FollowEmitter.");
+			return true;
+		}
+
+		return false;
+	}
+
+	/// <summary>Spawns a new GameObject.</summary>
+	/// <param name="Spawned">Outs the new GameObject.</param>
+	/// <param name="bTieToThis"><see langword="true"/> to parent <paramref name="Spawned"/> to this <see cref="AudioController"/>.</param>
 	void Spawn(out GameObject Spawned, bool bTieToThis)
 	{
 		Spawned = new GameObject();
@@ -164,7 +247,7 @@ public class AudioData
 	[Range(0, 1)] public float SpatialBlend = .75f;
 	[Range(0, 1.1f)] public float ReverbZoneMix = 1f;
 
-	/// <summary>Replicates <see cref="AudioSource"/>.</summary>
+	/// <summary>Replicates attaching an <see cref="AudioSource"/> component.</summary>
 	/// <param name="Emitter">Gets or attaches an <see cref="AudioSource"/> component.</param>
 	/// <param name="Source"><see langword="out"/> the attached <see cref="AudioSource"/> component.</param>
 	public void Construct(GameObject Emitter, out AudioSource Source)
