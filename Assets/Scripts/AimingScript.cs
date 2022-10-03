5aebceed7c49abc89a6d58057d852af2640dac77
Jenny <43059884+IkarosJenny@users.noreply.github.com>
2022-10-03 12:50:06 +1100

    not working

--- Assets/Scripts/AimingScript.cs
+++ Assets/Scripts/AimingScript.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class AimingScript : MonoBehaviour
 {
+    public Transform aimPosition;
+    //public GameObject currentGun;
+    GameObject currentTarget;
+
+    bool isAiming;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //currentGun.transform.position = aimPosition.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FindTarget();
+    }
+
+    private void FindTarget()
+    {
+        float closestDist = Mathf.Infinity;
+        GameObject closestObj = null;
+        GameObject[] allBreakables = GameObject.FindObjectsOfType<Breakable>();
+
+        foreach (GameObject breakableBlock in allBreakables) 
+        {
+            float breakableDist = (breakableBlock.tranform.position - this.transform.position).sqrMagnitude;
+            if (breakableDist < closestDist)
+            {
+                closestDist = breakableDist;
+                closestObj = currentTarget;
+            }
+        }
+        Debug.DrawLine(this.transform.position, closestObj.transform.position);
+
+        //RaycastHit hit;
+
+        //if (Physics.Raycast(transform.position, transform.forward, out hit, distance))
+        //{
+        //    if (hit.transform.gameObject.tag == "Breakable")
+        //    {
+        //        if (!isAiming)
+        //            Debug.Log("detected!!");
+
+        //        currentTarget = hit.transform.gameObject;
+        //        isAiming = true;
+        //    }
+        //    else
+        //    {
+        //        currentTarget = null;
+        //        isAiming = false;
+        //    }
+        //}
+    }
+
+    private void AutoAim() 
+    {
+        //currentGun.transform.LookAt(currentTarget.transform);
     }
 }
