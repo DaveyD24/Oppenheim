1b8fa9e64da4f6fa64b359cd93eb50ff6414bb4c
Jenny <43059884+IkarosJenny@users.noreply.github.com>
2022-10-09 12:19:55 +1100

    Cooldowns

--- Assets/Scripts/FuelScript.cs
+++ Assets/Scripts/FuelScript.cs
@@ -11,8 +11,15 @@ public class FuelScript : MonoBehaviour
     [SerializeField]
     private Image iconImg;
     [SerializeField]
+    private Image greyIcon;
+    [SerializeField]
     private Sprite monkeyIcon, soldierIcon, carIcon, batIcon;
 
+    //Get the player cooldowns
+    public float cooldown = 5f;
+    bool isCooldown = false;
+
+    //Get the player 
     //PlayerControllerScript currentPlayer
 
     private void Start() 
@@ -20,6 +27,8 @@ public class FuelScript : MonoBehaviour
         fuelBar = GetComponent<Image>();
         //currentPlayer = FindObjectOfType<PlayerControllerScript>();
         iconImg.sprite = monkeyIcon;
+
+
     }
 
     private void Update()
@@ -27,7 +36,7 @@ public class FuelScript : MonoBehaviour
         //currentFuel = currentPlayer.fuel;
         currentFuel = 50f;
         fuelBar.fillAmount = currentFuel / maxFuel;
-
+        playerSkill();
         /*
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
@@ -45,5 +54,21 @@ public class FuelScript : MonoBehaviour
         {
             iconImg.sprite = batIcon;
         }*/
+    }
+
+    private void playerSkill(){
+        if (Input.GetKeyDown(KeyCode.Space) && isCooldown == false){
+            isCooldown = true;
+            greyIcon.fillAmount = 1;
+        }
+
+        if (isCooldown){
+            greyIcon.fillAmount -= 1 / cooldown * Time.deltaTime;
+        }
+
+        if (greyIcon.fillAmount <= 0){
+            greyIcon.fillAmount = 0;
+            isCooldown = false;
+        }
     }
 }
