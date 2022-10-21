e94ca07a7014921a3b544c68b766c0ab8fea67b4
Jenny <43059884+IkarosJenny@users.noreply.github.com>
2022-10-21 13:34:46 +1100

    Autoaim - still cannot detect breakable blocks

--- Assets/Scripts/AimingScript.cs
+++ Assets/Scripts/AimingScript.cs
@@ -4,91 +4,72 @@ using UnityEngine;
 
 public class AimingScript : MonoBehaviour
 {
-    public Transform aimPosition;
     //public GameObject currentGun;
-    GameObject currentTarget;
-    float closestDist = Mathf.Infinity;
-    Collider closestObj = null;
-    Collider[] objNearby;
+    //public GameObject currentTarget;
+    [SerializeField] private GameObject fovStartPoint;
+    [SerializeField] private float lookSpeed;
+    [SerializeField] private float maxAngle = 90;
+    [SerializeField] private float closestDist = Mathf.Infinity;
+    [SerializeField] private GameObject closestObj = null;
+    private Collider[] objNearby;
 
-    bool isAiming;
-
-    void Start()
-    {
-        //currentGun.transform.position = aimPosition.position;
-    }
+    private Quaternion targetRotation;
+    private Quaternion lookAt;
 
     void Update()
     {
         FindTarget();
-    }
+        
+        //face direction if there is a breakable object in view, else just look forward
+        if (objInFieldOfView(fovStartPoint) && closestObj != null){
+            Vector3 direction = closestObj.transform.position - transform.position;
+            targetRotation = Quaternion.LookRotation(direction);
+            lookAt = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * lookSpeed);
+            transform.rotation = lookAt;
+        }
+        else{
+            targetRotation = Quaternion.Euler(0,0,0);
+            transform.localRotation = Quaternion.RotateTowards(
+                transform.localRotation, targetRotation, Time.deltaTime * lookSpeed);
+        }
 
+        
+    }
+    
+    //find closest object that is breakable
     private void FindTarget()
     {
-        /*
-        float closestDist = Mathf.Infinity;
-        GameObject closestObj = null;
-        GameObject[] allBreakables = GameObject.FindObjectsOfType<Breakable>();
-
-        foreach (GameObject breakableBlock in allBreakables) 
-        {
-            float breakableDist = (breakableBlock.tranform.position - this.transform.position).sqrMagnitude;
-            if (breakableDist < closestDist)
-            {
-                closestDist = breakableDist;
-                closestObj = currentTarget;
-            }
-        }
-        Debug.DrawLine(this.transform.position, closestObj.transform.position);*/
-
-
-
-
-
-
-
-        //RaycastHit hit;
-
-        //if (Physics.Raycast(transform.position, transform.forward, out hit, distance))
-        //{
-        //    if (hit.transform.gameObject.tag == "Breakable")
-        //    {
-        //        if (!isAiming)
-        //            Debug.Log("detected!!");
-
-        //        currentTarget = hit.transform.gameObject;
-        //        isAiming = true;
-        //    }
-        //    else
-        //    {
-        //        currentTarget = null;
-        //        isAiming = false;
-        //    }
-        //}
-
-
-        /*
-    private void FindTarget()
-        {
-        objNearby = Physics.OverlapSphere(this.soldierTransform.position, 50f);
+        objNearby = Physics.OverlapSphere(this.transform.position, 100f);
         foreach (Collider obj in objNearby) 
         {
-            if (obj.tag == "Breakable") {
-                float breakableDist = (obj.transform.position - this.transform.position).sqrMagnitude;
+            if (obj.GetComponent<Collider>().tag == "Breakable") {
+                float breakableDist = (obj.gameObject.transform.position - this.transform.position).sqrMagnitude;
+                Debug.Log("calculating breakable dis");
                 if (breakableDist < closestDist)
                 {
                     closestDist = breakableDist;
-                    closestObj = obj;
+                    closestObj = obj.gameObject;
+                    Debug.Log("THIS ONE CLOSER");
                 }
+            }
+            else{
+                Debug.Log("not breakable");
             }
         }
         Debug.DrawLine(this.transform.position, closestObj.transform.position);
     }
-        */
-    }
 
-    private void AutoAim() 
-    {
-        //currentGun.transform.LookAt(currentTarget.transform);
+    //check if the player POV can see the object
+    private bool objInFieldOfView(GameObject thePlayer){
+        
+        Vector3 targetDir = closestObj.transform.position - transform.position;
+        float angle = Vector3.Angle(targetDir, thePlayer.transform.forward);
+
+        if (angle < maxAngle){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
